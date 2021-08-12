6385fe8ae97d8a65d42f70b4fb2ee498da3f1560
Alison8547 <alisonailson100@hotmail.com>
2021-08-12 14:02:09 -0300

    Update-Lombok-constructor-autowired

--- src/main/java/com/course/personapi/controller/PersonController.java
+++ src/main/java/com/course/personapi/controller/PersonController.java
@@ -5,6 +5,7 @@ import com.course.personapi.dto.response.MessageResponseDTO;
 import com.course.personapi.dto.request.PersonDTO;
 import com.course.personapi.exception.PersonNotFoundException;
 import com.course.personapi.service.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,14 +15,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private PersonService personService;
-
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -30,7 +27,7 @@ public class PersonController {
     }
 
     @GetMapping
-    public List<PersonDTO> listAll(){ // Metodo post
+    public List<PersonDTO> listAll() { // Metodo post
         return personService.listAll();
     }
 
@@ -46,7 +43,7 @@ public class PersonController {
     }
 
     @PutMapping("/{id}")
-    public MessageResponseDTO updateById(@PathVariable Long id,@RequestBody @Valid PersonDTO personDTO) throws PersonNotFoundException {
+    public MessageResponseDTO updateById(@PathVariable Long id, @RequestBody @Valid PersonDTO personDTO) throws PersonNotFoundException {
         return personService.updateById(id, personDTO);
     }
 
