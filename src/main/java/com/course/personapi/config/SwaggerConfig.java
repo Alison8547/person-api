bc759e9a28c002bc972756d5851a1c2a90cdcf3c
Alison8547 <alisonailson100@hotmail.com>
2021-10-21 18:14:59 -0300

    Documentando-api-com-swagger

---
+++ src/main/java/com/course/personapi/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.course.personapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.course.personapi.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Person Api")
+                .description("Rest Api para gerenciamento de pessoas.")
+                .version("1.0.0")
+                .contact(new Contact("Alison", "https://github.com/Alison8547", null))
+                .build();
+    }
+}
